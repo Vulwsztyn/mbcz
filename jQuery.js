c64765dec32350490d5ea874b1e6ff7f86d415fa
Artur <artur.mostowski@op.pl>
2017-11-09 14:29:18 +0100

    first com

---
+++ jQuery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#fixedMenu").load("fixedmenu.html");
+    $("#part1").load("part1.html");
+    $("#part2").load("part2.html");
+    $("#part3").load("part3.html");
+});
---
+++ script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Artur on 2017-10-06.
+ */
